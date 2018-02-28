82b99b0685777456f78ee41351b938e782f2bd72
PRESTONPOWELL22 <preston@windowtorecovery.com>
2018-02-28 14:52:08 -0800

    filestructure basic layout and firebase added

---
+++ assets/javascript/logic.js
@@ -0,0 +1,12 @@
+
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAgI7AsjghhruTh2qcYW0Q6SEtw_TmAeuM",
+    authDomain: "train-schedule-bbc43.firebaseapp.com",
+    databaseURL: "https://train-schedule-bbc43.firebaseio.com",
+    projectId: "train-schedule-bbc43",
+    storageBucket: "train-schedule-bbc43.appspot.com",
+    messagingSenderId: "797806940418"
+  };
+  firebase.initializeApp(config);
